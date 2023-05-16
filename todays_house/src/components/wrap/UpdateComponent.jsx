5ada215c29c98bdf32b237402e5f5d4534fe25ad
HongHyukJin <gurwlszx@naver.com>
2023-05-17 02:08:14 +0900

    update form

--- todays_house/src/components/wrap/UpdateComponent.jsx
+++ todays_house/src/components/wrap/UpdateComponent.jsx
@@ -23,8 +23,8 @@ export default function UpdateComponent () {
                   <input type="text" name="email" id="email" className='half'/>
                   <span>@</span>
                   <input type="text" name="domain" id="domain" className='half'/>
+                  <p>이메일을 변경하시려면 운영자에게 메일을 보내주세요.</p>
                 </div>
-                <p>이메일을 변경하시려면 운영자에게 메일을 보내주세요.</p>
               </div>
               <div>
                 <div className='label'>
@@ -47,9 +47,11 @@ export default function UpdateComponent () {
                 <div className='label'>
                   <label htmlFor="">성별</label>
                 </div>
-                <div className='input'>
-                  <input type="radio" name="gender" id="gender1" value="남성" />남성
-                  <input type="radio" name="gender" id="gender2" value="여성" />여성
+                <div className='input-radio'>
+                  <ul>
+                    <li><input type="radio" name="gender" id="gender1" value="남성" />남성</li>
+                    <li><input type="radio" name="gender" id="gender2" value="여성" />여성</li>
+                  </ul>
                 </div>
               </div>
               <div>
@@ -64,6 +66,10 @@ export default function UpdateComponent () {
                 <div className='label'>
                   <label htmlFor="">프로필 이미지</label>
                 </div>
+                <div className="input">
+                  <input type="file" name="file" id="file" accept='image/*'/>
+                  {/* <img src="./img/avatar.avif" alt="" /> */}
+                </div>
               </div>
               <div>
                 <div className='label'>
@@ -73,6 +79,7 @@ export default function UpdateComponent () {
                   <input type="text" name="oneline" id="oneline" />
                 </div>
               </div>
+              <button type='submit'>회원 정보 수정</button>
             </form>
           </div>
         </div>
