3abd2a91827a845772291f74fccec5a0733d040c
soyeon98 <212psy@naver.com>
2023-05-16 22:01:02 +0900

    0516

--- todays_house/src/components/WrapComponent.jsx
+++ todays_house/src/components/WrapComponent.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import SignUpComponent from './wrap/SignUpComponent';
 
 export default function WrapComponent () {
   return (
     <div id="wrap">
-      
+      <SignUpComponent/>
     </div>
   );
 };
