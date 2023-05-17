13782916e658fb18292d38e3e53ec2fac3129df6
Younghyeon22 <marinoma@naver.com>
2023-05-17 17:52:47 +0900

    2023-05-17

--- todays_house/src/components/WrapComponent.jsx
+++ todays_house/src/components/WrapComponent.jsx
@@ -3,6 +3,8 @@ import TopmodalComponent from './wrap/TopmodalComponent';
 import HeaderComponent from './wrap/HeaderComponent';
 import FooterComponent from './wrap/FooterComponent';
 import Section1Component from './wrap/Section1Component';
+import Section2Component from './wrap/Section2Component';
+import Section3Component from './wrap/Section3Component';
 
 
 export default function WrapComponent () {
@@ -11,6 +13,8 @@ export default function WrapComponent () {
          <TopmodalComponent/>
          <HeaderComponent/>
          <Section1Component/>
+         <Section2Component/>
+         <Section3Component/>
          <FooterComponent/>
     </div>
   );
--- todays_house/src/components/wrap/Section1Component.jsx
+++ todays_house/src/components/wrap/Section1Component.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 export default  function Section1Component(){
     return (
         <div id='section1'>
-            
+            <div className="container">
+                <div className="gap">
+                    <div className="content">
+                        <div className="left">
+                            <a href="!#"><img src="./images/166364535623611806.jpg" alt="" /></a>
+                        </div>
+                        <div className="right">
+                            <a href="!#"><img src="./images/168016842438804733.png" alt="" /></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
