42854f8675aed9ee2446235d6cb3bd99c9b23947
DDULKI <seulki4994@naver.com>
2023-05-17 21:48:29 +0900

    0517 로그인..재설정..하

--- todays_house/src/components/WrapComponent.jsx
+++ todays_house/src/components/WrapComponent.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SigninComponent from './wrap/SigninComponent'
+import EmailComponent from './wrap/EmailComponent'
 
 export default function WrapComponent () {
   return (
     <div id="wrap">
       <SigninComponent/>
+      <EmailComponent/>
     </div>
   );
 };
