483acb9981cf78d8c34e4e2eb5ed336059cb2c5f
soyeon98 <212psy@naver.com>
2023-05-17 21:49:15 +0900

    0517

--- todays_house/src/components/WrapComponent.jsx
+++ todays_house/src/components/WrapComponent.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import SignUpComponent from './wrap/SignUpComponent';
+import MyPageComponent from './wrap/MyPageComponent';
 
 export default function WrapComponent () {
   return (
     <div id="wrap">
+      <MyPageComponent/>
       <SignUpComponent/>
     </div>
   );
