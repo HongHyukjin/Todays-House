9ad84ef13116d2386ca2203b08bdab3fad5b6a39
DDULKI <seulki4994@naver.com>
2023-05-21 18:18:06 +0900

    0521 dto dao

--- todays_house/src/components/WrapComponent.jsx
+++ todays_house/src/components/WrapComponent.jsx
@@ -1,15 +1,56 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route}  from 'react-router-dom';
+import EmailComponent from './wrap/EmailComponent';
+import IntroComponent from './wrap/IntroComponent';
 import SigninComponent from './wrap/SigninComponent'
-import EmailComponent from './wrap/EmailComponent'
+import SignUpComponent from './wrap/SignUpComponent';
+import UploadPhotoComponent from './wrap/mypage/UploadPhotoComponent';
+
 
 export default function WrapComponent () {
+
+  React.useEffect(() => {
+    console.log(`                                                                        ,,
+                                                                           /  ,
+                                                                          /   /
+                                                                         /   /
+                                                                        /   /
+         __________________________                                    /   /
+        ⎢                         ⎥                                   /   /
+        ⎢  누구나 예쁜 집에 살 수 있어  ⎥                                  /   /
+        ⎢____    _________________⎥                                 /   /
+              \\/    ,      ,,                                      /   /
+                   /@\\____/@ \\                                ____/   /
+                  /           \\                         _____/        /_
+            /" \\ / •    •      \\                     __/             /  @@\\"\\
+            \\    @@  ㅅ  @@     /___             ___/                /    _/
+           /" \\   \\                 \\__________/                    |__/ "\\
+           \\   \\                                                   /      /
+            \\    \\  __                                                  _/
+             \\                                                       __/
+               \\_                                             ______/
+                  \\ ___                                    __/
+                        \\__                             __/
+                            \\_____                _____/
+                                  \\______________/
+                                  `);
+    console.log("");
+
+  }, [])
+
   return (
     <div id="wrap">
-      <Routes>
-        <SigninComponent/>
-        <EmailComponent/>
-      </Routes>
+
+         <BrowserRouter>
+            <Routes>
+                    <Route path='/*' element={<IntroComponent />} />
+                    <Route path="/로그인" element={<SigninComponent />}/>
+                    <Route path="/회원가입" element={<SignUpComponent />}/>
+                    <Route path="/비밀번호재설정" element={<EmailComponent />}/>
+                    <Route path="/사진업로드" element={<UploadPhotoComponent />}/>
+            </Routes>
+         </BrowserRouter>
+
     </div>
   );
 };
---
+++ todays_house/src/components/wrap/main/Section7Component.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+ 
+export default function Section7Component(){
+    return (
+        <div id='section7'>
+            <div className="container">
+                <div className="gap">
+                    <div className="title">
+                        <h2>카테고리별 상품 찾기</h2>
+                    </div>
+                    <div className="content">
+                        <div className="baner-box">
+                            <ul>
+                                <li>
+                                    <a href="!#"><img src="./images/165698403420736265.png" alt="" /></a>
+                                    <span>가구</span>
+                                </li>
+                                <li>
+                                    <a href="!#"><img src="./images/166010553870408108.png" alt="" /></a>
+                                    <span>패브릭</span>
+                                </li>
+                                <li>
+                                    <a href="!#"><img src="./images/166072985530266073.png" alt="" /></a>
+                                    <span>가전·디지털</span>
+                                </li>
+                                <li>
+                                    <a href="!#"><img src="./images/165443714334272236.png" alt="" /></a>
+                                    <span>주방용품</span>
+                                </li>
+                                <li>
+                                    <a href="!#"><img src="./images/166727583625644946.png" alt="" /></a>
+                                    <span>식품</span>
+                                </li>
+                                <li>
+                                    <a href="!#"><img src="./images/167167019685810067.png" alt="" /></a>
+                                    <span>데코·식물</span>
+                                </li>
+                                <li>
+                                    <a href="!#"><img src="./images/165443713935676588.png" alt="" /></a>
+                                    <span>조명</span>
+                                </li>
+                                <li>
+                                    <a href="!#"><img src="./images/165443714917341657.png" alt="" /></a>
+                                    <span>수납·정리</span>
+                                </li>
+                                <li>
+                                    <a href="!#"><img src="./images/165519431107541334.png" alt="" /></a>
+                                    <span>생활용품</span>
+                                </li>
+                                <li>
+                                    <a href="!#"><img src="./images/165579048228622395.png" alt="" /></a>
+                                    <span>생필품</span>
+                                </li>
+                                <li>
+                                    <a href="!#"><img src="./images/166598560990637620.png" alt="" /></a>
+                                    <span>유아·아동</span>
+                                </li>
+                                <li>
+                                    <a href="!#"><img src="./images/162823231401891024.png" alt="" /></a>
+                                    <span>반려동물</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
