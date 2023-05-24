d6fb66fb12750d3ba2230fff47f50a8d378a2a0c
DDULKI <seulki4994@naver.com>
2023-05-24 09:51:35 +0900

    0524 랜덤번호 버전

--- todays_house/src/components/wrap/email.js
+++ todays_house/src/components/wrap/email.js
@@ -1,3 +1,40 @@
+// 텍스트버전 
+
+// const nodeMailer = require('nodemailer');
+
+// const email = {
+//     service: 'Naver',
+//     host: 'smtp.naver.com',
+//     port: 465,
+//     auth: {
+//     user: 'seulki4994@naver.com',
+//     pass: 'tmfrl4994'
+//     }
+// }
+
+// const send = async(data) =>{
+//     nodeMailer.createTransport(email).sendMail(data, function(error, info){
+//       if(error) {
+//         console.log(error);
+//       }
+//       else {
+//         console.log(info);
+//         return info.response;
+//       }
+//     });
+// };
+
+// const content = {
+//   from: "seulki4994@naver.com",
+//   to: "seulki4994@naver.com",
+//   subject: "감사합니다^^^^^^^^^^^^^^.",
+//   text : "풀스택 - 오늘의집 이메일 인증 버튼 구현 "
+// }
+
+// send(content);
+
+
+// 랜덤번호 버전 
 const nodeMailer = require('nodemailer');
 
 const email = {
@@ -5,28 +42,31 @@ const email = {
     host: 'smtp.naver.com',
     port: 465,
     auth: {
-    user: 'seulki4994@naver.com',
-    pass: 'tmfrl4994'
+        user: 'seulki4994@naver.com',
+        pass: 'tmfrl4994'
     }
-}
+};
 
-const send = async(data) =>{
-    nodeMailer.createTransport(email).sendMail(data, function(error, info){
-      if(error) {
-        console.log(error);
-      }
-      else {
-        console.log(info);
-        return info.response;
-      }
+const send = async (data) => {
+    nodeMailer.createTransport(email).sendMail(data, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(info);
+            return info.response;
+        }
     });
 };
 
+const generateRandomNumber = () => {
+    return Math.floor(Math.random() * 1000000).toString();
+};
+
 const content = {
-  from: "seulki4994@naver.com",
-  to: "seulki4994@naver.com",
-  subject: "감사합니다^^^^^^^^^^^^^^.",
-  text : "풀스택 - 오늘의집 이메일 인증 버튼 구현 "
-}
+    from: "seulki4994@naver.com",
+    to: "seulki4994@naver.com",
+    subject: "오늘의집 가입을 환영합니다:)♥",
+    text: generateRandomNumber() // 랜덤 번호 생성
+};
 
 send(content);
